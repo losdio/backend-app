d39629f1be3a3ab7ab3e66272f15dfed5376924a
losdio <alzabintareq1@gmai.com>
2024-10-29 08:22:40 +0300

    first commit

---
+++ Controllers/TestController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using backend_app.Models;
+
+namespace backend_app.Controllers
+{
+    [RoutePrefix("api/Test")]
+    public class TestController : ApiController
+    {
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString);
+        SqlCommand cmd = null;
+        SqlDataAdapter da = null;
+
+        [HttpPost]
+        [Route("Registration")]
+        public string Registration(User user)
+        {
+            string msg = string.Empty;
+            try
+            {
+                cmd = new SqlCommand("usp_Registration", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Username", user.Username);
+                cmd.Parameters.AddWithValue("@Name", user.Name);
+                cmd.Parameters.AddWithValue("@Email", user.Email);
+                cmd.Parameters.AddWithValue("@Password", user.Password);
+
+                conn.Open();
+                int i = cmd.ExecuteNonQuery();
+                conn.Close();
+
+                if (i > 0)
+                {
+                    msg = "Success";
+                }
+                else
+                {
+                    msg = "User already registered.";
+                }
+            }
+            catch (Exception ex) 
+            { 
+                msg = ex.Message;
+            }
+            
+            return msg;
+        }
+        [HttpPost]
+        [Route("Login")]
+        public string Login(User user)
+        {
+            string msg = string.Empty;
+            try
+            {
+                da = new SqlDataAdapter("usp_Login", conn);
+                da.SelectCommand.CommandType = CommandType.StoredProcedure;
+                da.SelectCommand.Parameters.AddWithValue("@Username", user.Username);
+                da.SelectCommand.Parameters.AddWithValue("@Password", user.Password);
+                DataTable dataTable = new DataTable();
+                da.Fill(dataTable);
+                if (dataTable.Rows.Count > 0) { 
+                    msg = "Valid";
+                }
+                else
+                {
+                    msg = "Invalid";
+                }
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+            }
+
+            return msg;
+        }
+    }
+}
